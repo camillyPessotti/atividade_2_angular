30e57b5ea3ab53408efa4ef875c631c0dd3be055
Your Name <92130029+camillyPessotti@users.noreply.github.com>
2021-12-16 21:56:07 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
@@ -12,6 +13,11 @@ import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.co
     PaginaPrincipalComponent
   ],
   imports: [
+    RouterModule.forRoot([
+      {
+        path: '', component: LoginComponent, canActivate: []
+      }
+    ]),
     BrowserModule
   ],
   providers: [],
